658bc872b181720a777d04b72895e36aabb4bf5b
Rayarise <rayarise23@gmail.com>
2020-09-15 14:31:41 -0400

    Commit final

--- FileScanner/ViewModels/MainViewModel.cs
+++ FileScanner/ViewModels/MainViewModel.cs
@@ -10,38 +10,77 @@ using System.Windows.Forms;
 
 namespace FileScanner.ViewModels
 {
-    public class MainViewModel : BaseViewModel
+    public class Items : BaseViewModel
     {
-        private string selectedFolder;
-        private ObservableCollection<string> folderItems = new ObservableCollection<string>();
-         
-        public DelegateCommand<string> OpenFolderCommand { get; private set; }
-        public DelegateCommand<string> ScanFolderCommand { get; private set; }
 
-        public ObservableCollection<string> FolderItems { 
+        private string folderItems;
+
+        private string image;
+
+        public string FolderItems
+        {
             get => folderItems;
-            set 
-            { 
+            set
+            {
                 folderItems = value;
                 OnPropertyChanged();
             }
         }
 
-        public string SelectedFolder
+
+        public string Image
         {
-            get => selectedFolder;
+            get => image;
             set
             {
-                selectedFolder = value;
+               image = value;
                 OnPropertyChanged();
-                ScanFolderCommand.RaiseCanExecuteChanged();
             }
         }
+
+
+
+    }
+    public class MainViewModel : BaseViewModel
+    {
+
+        public DelegateCommand<string> OpenFolderCommand { get; private set; }
+        public DelegateCommand<string> ScanFolderCommand { get; private set; }
+
+        private string selectedFolder;
+
+        private ObservableCollection<Items> itemsF = new ObservableCollection<Items>();
+
+
+        public ObservableCollection<Items> ItemsF
+        {
+            get => itemsF;
+            set
+            {
+                itemsF = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+      
+
+        public string SelectedFolder
+          {
+              get => selectedFolder;
+              set
+              {
+                  selectedFolder = value;
+                  OnPropertyChanged();
+                  ScanFolderCommand.RaiseCanExecuteChanged();
+              }
+          }
 
         public MainViewModel()
         {
             OpenFolderCommand = new DelegateCommand<string>(OpenFolder);
-            ScanFolderCommand = new DelegateCommand<string>(ScanFolder, CanExecuteScanFolder);
+            ScanFolderCommand = new DelegateCommand<string> (ScanFolderAsync, CanExecuteScanFolder);
+          
         }
 
         private bool CanExecuteScanFolder(string obj)
@@ -51,32 +90,69 @@ namespace FileScanner.ViewModels
 
         private void OpenFolder(string obj)
         {
-            using (var fbd = new FolderBrowserDialog())
-            {
-                if (fbd.ShowDialog() == DialogResult.OK)
+         
+                using (var fbd = new FolderBrowserDialog())
                 {
-                    SelectedFolder = fbd.SelectedPath;
+                    if (fbd.ShowDialog() == DialogResult.OK)
+                    {
+                        SelectedFolder = fbd.SelectedPath;
+                    }
+                }
+         
+        }
+
+     
+       
+        private async void ScanFolderAsync(string dir)
+        {
+             
+            try
+            {
+               
+                ItemsF = new ObservableCollection<Items>(await Task.Run(() => GetDirs(dir)));
+
+               
+
+                foreach (var item in Directory.EnumerateFiles(dir, "*"))
+                {
+                    Items it = new Items();
+                    it.FolderItems = item;
+                    it.Image = "/Image/file.png";
+                    ItemsF.Add(it);
+                    Console.Write(it.FolderItems);
                 }
             }
-        }
+            catch(Exception) {
 
-        private void ScanFolder(string dir)
-        {
-            FolderItems = new ObservableCollection<string>(GetDirs(dir));
-            
-            foreach (var item in Directory.EnumerateFiles(dir, "*"))
-            {
-                FolderItems.Add(item);
+                Items it = new Items();
+                it.FolderItems = "you don't have the permissions to scan this file.";
+                it.Image = "";
+                itemsF.Clear();
+                ItemsF.Add(it);
             }
+            
         }
 
-        IEnumerable<string> GetDirs(string dir)
-        {            
+      
+
+          
+        
+
+         IEnumerable<Items> GetDirs(string dir)
+        {
+
+          
             foreach (var d in Directory.EnumerateDirectories(dir, "*"))
             {
-                yield return d;
+                Items it = new Items();
+                it.FolderItems = d;
+                it.Image = "/Image/dossier.png";
+                yield return it;
+               
             }
         }
+
+     
 
         ///TODO : Tester avec un dossier avec beaucoup de fichier
         ///TODO : Rendre l'application asynchrone
